79d018629bf7b7cadc12f44ca3abdfd3462db941
Marc Dusseiller <marc@dusseiller.ch>
2022-04-30 16:59:01 +0700

    Delete secrets.h

--- software/ESP32-S2/featherS2_Thingspeak_SCD41/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "Marcjono Lab"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "sepuluribu"  // replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 1718702      // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "O9HG7LC5CKDJEW48"   // replace XYZ with your channel write API Key
